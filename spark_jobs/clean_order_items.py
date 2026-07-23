cb277c0c29c763a02b77fb36635441042b82f11f
daudabd <dabdinasir7213@sdsu.edu>
2026-07-22 17:49:59 -0700

    Update paths for WSL environment on Dell

--- spark_jobs/clean_order_items.py
+++ spark_jobs/clean_order_items.py
@@ -5,12 +5,12 @@ from pyspark.sql.functions import col
 # Initialize Spark using the internal container path for the JDBC jar
 spark = (SparkSession.builder
         .appName("OrderItems")
-        .config("spark.jars", "/opt/airflow/jdbc_driver/postgresql-42.7.11.jar")
+        .config("spark.jars", "/home/daudabd/jdbc_drivers/postgresql-42.7.11.jar")
         .getOrCreate()
 )
 
 # Read from the container's mounted data folder
-df = spark.read.option("header", True).option("inferSchema", True).csv("/opt/airflow/data/raw/olist_order_items_dataset.csv")
+df = spark.read.option("header", True).option("inferSchema", True).csv("/home/daudabd/de-pipeline-olist/data/raw/olist_order_items_dataset.csv")
 
 # Transformations
 df_clean = (df
@@ -22,7 +22,7 @@ df_deduped = df_clean.drop_duplicates(["order_id", "order_item_id"])
 # Database configurations pointing to the internal Docker service network
 db_user = os.environ.get("OLIST_DB_USER", "olist_user")
 db_password = os.environ.get("OLIST_DB_PASSWORD", "olist_password")
-jdbc_url = "jdbc:postgresql://olist-postgres:5432/olist_db"
+jdbc_url = "jdbc:postgresql://localhost:5433/olist_db"
 
 # Write out to the warehouse target
 df_deduped.write.jdbc(
